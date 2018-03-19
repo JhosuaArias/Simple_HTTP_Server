6b1e5236a199da6e923e7830861b0b956e615b93
Carlos Gamboa <cali1997gv@hotmail.com>
2018-03-19 10:37:33 -0600

    Some Class constructors

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,28 @@
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 public class Controller {
 
+    Server server;
+    Terminal terminal;
 
+    public Controller(){
 
-    public static void main(String[] args) {
+        this.server = new Server();
+        this.terminal = new Terminal();
+
+    }
+
+    private void init () throws Exception{
+
+        this.server.start();
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Controller controller = new Controller();
+        controller.init();
 
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -4,7 +4,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String args[]) throws Exception {
+
+    HttpHandler httpHandler;
+
+    public void start () throws Exception {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
