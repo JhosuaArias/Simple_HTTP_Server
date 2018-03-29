03366f4c661fe64b5b977781a33c89cb7a08a922
carlos-gamboa <cali1997gv@hotmail.com>
2018-03-29 17:54:32 -0600

    Handle Log

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -3,6 +3,8 @@ import java.util.Map;
 public class HttpRequest {
 
     String filename;
+    String host;
+    String referer;
     Method method;
     Map<String, String> parameters;
 
--- src/Method.java
+++ src/Method.java
@@ -1,7 +1,17 @@
 public enum Method {
-    POST,
-    GET,
-    HEAD,
-    PUT,
-    UNKWOWN
+    POST ("POST"),
+    GET ("GET"),
+    HEAD ("HEAD"),
+    PUT ("PUT"),
+    UNKWOWN ("UNKNOWN");
+
+    private String name;
+
+    Method(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
