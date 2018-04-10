b2f636b2594a1204f2b931b99f51628dedb9ae03
JhosuaArias <jhosuarqui@hotmail.com>
2018-04-10 16:39:05 -0600

    HttpHandler

--- src/HttpHandler.java
+++ src/HttpHandler.java
@@ -1,22 +1,85 @@
 import java.io.FileReader;
+import java.util.HashMap;
 
 public class HttpHandler {
 
-    public HttpHandler(){
-
+    private MimeTypes mimeTypes;
+    private HttpRequest httpRequest;
+    public HttpHandler(MimeTypes mimeTypes) {
+        this.mimeTypes = mimeTypes;
     }
 
-    public  void handleHttp(String message) {
+    public void handleHttp(String message) {
+
+        HashMap<String, String> hashHeaders = new HashMap<>();
+        HashMap<String, String> hashParameters = new HashMap<>();
+        String splitMessage[] = message.split("\n");
+        String splitFirstLine[] = splitMessage[0].split("\\s+");
+        String method = splitFirstLine[0];
+        String parameters[];
+        String filename;
+
+        for (int i = 1; i < splitMessage.length; i++) {
+            String splitHeader[] = splitMessage[i].split(":");
+            try {
+                hashHeaders.put(splitHeader[0].trim(), splitHeader[1].trim());
+            } catch (Exception ignored) {
+            }
+        }
+
+        String referer = hashHeaders.get("Referer");
+        if (referer == null) {
+            referer = "";
+        }
+
+        if (method.equalsIgnoreCase("GET") || method.equalsIgnoreCase("HEAD")) { //Como eran lo mismo
+            String splitUrl[] = splitFirstLine[1].split("\\?");
+            filename = splitUrl[0];
+
+            if(filename.equalsIgnoreCase("/")){
+                filename = "/index.html";
+            }
+
+            if (splitUrl.length == 2) {
+                parameters = splitUrl[1].split("&");
+
+                for (int i = 0; i < parameters.length; i++) {
+                    String parameter[] = parameters[i].split("=");
+                    hashParameters.put(parameter[0],parameter[1]);
+                }
+            }
+            System.out.println("filename: " + filename + "\nparameters: " + hashParameters.toString());
+            if (method.equalsIgnoreCase("GET")) {
+                httpRequest = new HttpRequest(filename, hashHeaders.get("Host"), referer, Method.GET, hashParameters);
+            } else {
+                httpRequest = new HttpRequest(filename, hashHeaders.get("Host"), referer, Method.HEAD, hashParameters);
+            }
+        } else {
+
+            filename = splitMessage[1];
+            if (method.equalsIgnoreCase("POST")) {
+                if(filename.equalsIgnoreCase("/")){
+                    filename = "index.html";
+                }
+                parameters = splitMessage[splitMessage.length-1].split("&");
+                for (int i = 0; i < parameters.length; i++) {
+                    String parameter[] = parameters[i].split("=");
+                    hashParameters.put(parameter[0],parameter[1]);
+                }
+                httpRequest = new HttpRequest(filename, hashHeaders.get("Host"), referer, Method.UNKWOWN, hashParameters);
+            } else {
+                httpRequest = new HttpRequest(filename, hashHeaders.get("Host"), referer, Method.UNKWOWN, hashParameters);
+            }
+        }
 
     }
 
     public String createResponse() {
-        return null;
+        HttpResponse httpResponse = new HttpResponse(this.httpRequest);
+        return httpResponse.getResponse();
     }
 
     public String errorHandler() {
         return null;
     }
-
-
 }
--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -8,4 +8,15 @@ public class HttpRequest {
     Method method;
     Map<String, String> parameters;
 
+
+    public HttpRequest(String filename, String host, String referer, Method method, Map<String, String> parameters) {
+        this.filename = filename;
+        this.host = host;
+        this.referer = referer;
+        this.method = method;
+        this.parameters = parameters;
+    }
+
+
+
 }
